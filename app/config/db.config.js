73322bec91339cb5545c591970d4b516324f3daa
VuTranAnh <vu.ta183674@sis.hust.edu.vn>
2023-08-02 18:30:11 +0700

    update db environment config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  url: `mongodb://${process.env.DB_USE_USR_PASS==='1' ? `${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@`: ``}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 };
